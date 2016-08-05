2419961a2f2c8a779baae4b7a7de8409d9fd3d59
Martin Ciparelli <mciparelli@gmail.com>
2016-08-05 15:13:12 -0300

    Add bundle

--- src/router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'director';
-
-export default State => Router({
-  '/(active|completed|)': filter => {
-    const state = State.get();
-    if (filter === '') return state.ui.remove('filter');
-    return state.ui.set({ filter });
-  }
-});
